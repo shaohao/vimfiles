68f30fa8aeab4d3fc11fdd1ca02c887f7d1ea6a5
Shao Hao <shaohao@live.cn>
2013-10-04 20:26:33 +0800

    add update.sh.

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git subtree pull --prefix=bundle/neocomplcache https://github.com/Shougo/neocomplcache.vim.git master --squash
+git subtree pull --prefix=bundle/syntastic     https://github.com/scrooloose/syntastic.git     master --squash
+git subtree pull --prefix=bundle/tagbar        https://github.com/majutsushi/tagbar.git        master --squash
+git subtree pull --prefix=bundle/vim-surround  https://github.com/tpope/vim-surround.git       master --squash
+git subtree pull --prefix=bundle/vim-pathogen  https://github.com/tpope/vim-pathogen.git       master --squash
+git subtree pull --prefix=bundle/vcscommand    git://repo.or.cz/vcscommand.git                 master --squash
