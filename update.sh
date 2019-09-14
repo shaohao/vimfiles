14253bca5982c6042208835d8481af8f3d7f762f
Shao Hao <shaohao@outlook.com>
2019-09-14 18:47:22 +0800

    add VisIncr.

--- update.sh
+++ update.sh
@@ -9,3 +9,4 @@ git subtree pull --prefix=bundle/vim-surround    https://github.com/tpope/vim-su
 git subtree pull --prefix=bundle/vim-pathogen    https://github.com/tpope/vim-pathogen.git         master --squash
 git subtree pull --prefix=bundle/ack.vim         https://github.com/mileszs/ack.vim.git            master --squash
 git subtree pull --prefix=bundle/vcscommand.vim  https://github.com/vim-scripts/vcscommand.vim.git master --squash
+git subtree pull --prefix=bundle/VisIncr         https://github.com/vim-scripts/VisIncr.git        master --squash
